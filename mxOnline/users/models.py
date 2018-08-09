e30752a9d2540f340af8d8f51d25920f2344658d
ouru <18719091650@163.com>
2018-08-09 14:50:42 +0800

    add

---
+++ mxOnline/users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50, verbose_name="昵称", default="")
+    birthday = models.DateField(verbose_name="生日", null=True, blank=True)
+    gender = models.CharField(max_length=6, verbose_name="性别", choices=(("male", "男"), ("female", "女")), default="female")
+    address = models.CharField(max_length=100, default="")
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to="images/%Y/%m", default="images/default.jpg", max_length=100)
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
--- novel_site/novel/urls.py
+++ novel_site/novel/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 from . import views
 
 app_name = "novel"
@@ -6,5 +7,5 @@ app_name = "novel"
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^(\d+)/$', views.novel_detail, name="novel_detail"),
-    url(r'^(\d+)/(\d+)/$', views.chapter_detail, name="chapter_detail"),
+    url(r'^(\d+)/(\d+)/$', cache_page(60*15)(views.chapter_detail), name="chapter_detail"),
 ]
